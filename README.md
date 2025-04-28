179585ffc9cfebb0910b9a27d9e31db5086227c3
simone <simone.mapelli@abenergie.it>
2025-04-28 13:36:07 +0200

    project creation

--- README.md
+++
@@ -1,1 +0,0 @@
-# FreddyFridge_template
---
+++ app/src/main/java/eu/indiewalkabout/fridgemanager/MainActivity.kt
@@ -0,0 +1,47 @@
+package eu.indiewalkabout.fridgemanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import eu.indiewalkabout.fridgemanager.ui.theme.FreddyFridge_StartingOverTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FreddyFridge_StartingOverTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FreddyFridge_StartingOverTheme {
+        Greeting("Android")
+    }
+}
